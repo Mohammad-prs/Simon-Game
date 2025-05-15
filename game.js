fa0050e5e4165fd9e43dba3b76d81b0e02fb93f7
mohammad-prs <mparsafard@yahoo.com>
2025-05-15 02:52:34 -0400

    make the game responsive on phone and also added the start and restart button

--- game.js
+++ game.js
@@ -1,82 +1,84 @@
-var colors = ["green" , "red" , "blue" , "yellow"] ;
-var gamePattern = [] ;
+var colors = ["green", "red", "blue", "yellow"];
+var gamePattern = [];
 var usersClickedPattern = [];
-var level = 0 ;
-var started = false ;
+var level = 0;
+var started = false;
 
-
-$(document).on("keydown" , function () {
+$("#start-btn").on("click", function () {
     if (!started) {
-        started = true ;
-        nextSequence ();
-        $("#level-title").html("Level " + level) ;
+        started = true;
+        level = 0;
+        gamePattern = [];
+        usersClickedPattern = [];
+        nextSequence();
+        $("#level-title").text("Level " + level);
     }
-})
+});
 
+$("#restart-btn").on("click", function () {
+    startOver();
+    $("#level-title").text("Press Start to Begin");
+});
 
+function nextSequence() {
+    usersClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
 
-function nextSequence () {
-    usersClickedPattern = [] ;
-    level++ ;
-    $("#level-title").html("Level " + level) ;
-    var randomIndex = Math.floor(Math.random() * 4 ) ;
-    var randomColorChosen = colors[randomIndex] ;
-    gamePattern.push(randomColorChosen) ;
-    $("#" + randomColorChosen).fadeIn(100).fadeOut(100).fadeIn(100) ;
-    playSound(randomColorChosen) ;
-    animatePressed(randomColorChosen) ;
+    var randomIndex = Math.floor(Math.random() * 4);
+    var randomColorChosen = colors[randomIndex];
+    gamePattern.push(randomColorChosen);
+
+    $("#" + randomColorChosen).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomColorChosen);
+    animatePressed(randomColorChosen);
 }
 
-$(".btn").on("click" , function () {
-    var usersChosenColor = $(this).attr("id") ;
-    usersClickedPattern.push(usersChosenColor) ;
-    playSound(usersChosenColor) ;
-    animatePressed(usersChosenColor) ;
-    checkAnswer(usersClickedPattern.length-1) ; 
-})
+$(".btn").on("click", function () {
+    if (!started) return; // prevent clicking before start
 
-function checkAnswer (currentLevel) {
+    var userChosenColor = $(this).attr("id");
+    usersClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePressed(userChosenColor);
+    checkAnswer(usersClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === usersClickedPattern[currentLevel]) {
-        console.log("succeed") ;
-
-        if(gamePattern.length === usersClickedPattern.length) {
+        if (usersClickedPattern.length === gamePattern.length) {
             setTimeout(function () {
-                nextSequence ()
-            } ,1000 ) ;
+                nextSequence();
+            }, 1000);
         }
     } else {
-        var lostAudio = new Audio('sounds/wrong.mp3') ;
-        lostAudio.play () ;
+        playSound("wrong");
 
-        $("body").addClass("game-over") ;
+        $("body").addClass("game-over");
         setTimeout(function () {
-            $("body").removeClass("game-over") ;
-        } , 200) ;
+            $("body").removeClass("game-over");
+        }, 200);
 
-        $("#level-title").html("Game Over, Press Any Key To Restart")
-         
-        startOver () ;
+        $("#level-title").text("Game Over! Press Restart");
+        started = false;
     }
-
 }
 
-
-function startOver () {
-    level = 0 ;
-    gamePattern = [] ;
-    started = false ;
+function startOver() {
+    started = false;
+    level = 0;
+    gamePattern = [];
+    usersClickedPattern = [];
 }
 
-function playSound (name) {
-    var audio = new Audio("sounds/" + name + ".mp3") ;
-    audio.play() ;
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
-function animatePressed (currentColor) {
-    $("#" + currentColor).addClass("pressed") ;
+function animatePressed(color) {
+    $("#" + color).addClass("pressed");
     setTimeout(function () {
-        $("#" + currentColor).removeClass("pressed") ;        
+        $("#" + color).removeClass("pressed");
     }, 100);
 }
-
-
